acaa8017b56960d17de74217cc0cc118e152da5c
Q <q@misell.cymru>
2022-10-20 17:08:04 +0100

    normalise GTINs

--- src/barcode.rs
+++ src/barcode.rs
@@ -1,86 +1,30 @@
 #[derive(Debug, PartialEq, Eq, Hash, Serialize, Deserialize, Clone)]
-pub enum Barcode {
-    Ean13(Vec<u8>),
-    Ean8(Vec<u8>),
-    UpcA(Vec<u8>),
-    UpcE(Vec<u8>),
-}
+pub struct Barcode([u8; 14]);
 
 impl std::fmt::Display for Barcode {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Barcode::Ean13(digits) => {
-                write!(f, "EAN-13: {}", digits.iter().map(|d| d.to_string()).collect::<String>())
-            }
-            Barcode::Ean8(digits) => {
-                write!(f, "EAN-8: {}", digits.iter().map(|d| d.to_string()).collect::<String>())
-            }
-            Barcode::UpcA(digits) => {
-                write!(f, "UPC-A: {}", digits.iter().map(|d| d.to_string()).collect::<String>())
-            }
-            Barcode::UpcE(digits) => {
-                write!(f, "UPC-E: {}", digits.iter().map(|d| d.to_string()).collect::<String>())
-            }
-        }
+        write!(f, "{}", self.0.iter().map(|d| d.to_string()).collect::<String>())
     }
 }
 
 impl Barcode {
     pub fn try_parse(input: &str) -> Option<Self> {
-        let barcode_digits = int_digits(input)?;
-        match barcode_digits.len() {
-            13 => Some(Self::Ean13(barcode_digits)),
-            12 => Some(Self::UpcA(barcode_digits)),
-            8 => Some(Self::Ean8(barcode_digits)),
-            6 => Some(Self::UpcE(barcode_digits)),
+        let d = int_digits(input)?;
+        match d.len() {
+            14 => Some(Self([d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10], d[11], d[12], d[13]])),
+            13 => Some(Self([0, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10], d[11], d[12]])),
+            12 => Some(Self([0, 0, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10], d[11]])),
+            8 => Some(Self([0, 0, 0, 0, 0, 0, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]])),
+            6 => Some(Self([0, 0, 0, 0, 0, 0, 0, 0, d[0], d[1], d[2], d[3], d[4], d[5]])),
             _ => None
         }
     }
 
     pub fn check_digit(&self) -> bool {
-        let check = |digits: &[u8], invert: bool| -> bool {
-            let (odd, even): (Vec<_>, Vec<_>) = digits[..]
-                .iter().enumerate()
-                .partition(|&x| if invert {
-                    x.0 % 2 == 0
-                } else {
-                    x.0 % 2 == 1
-                });
-            let sum = even.iter().map(|x| *x.1 as u32).sum::<u32>() +
-                (odd.iter().map(|x| *x.1 as u32).sum::<u32>() * 3);
-            sum % 10 == 0
-        };
-
-        match self {
-            Barcode::Ean13(digits) => {
-                if digits.len() != 13 {
-                    return false;
-                }
-
-                check(digits, false)
-            }
-            Barcode::Ean8(digits) => {
-                if digits.len() != 8 {
-                    return false;
-                }
-
-                check(digits, true)
-            }
-            Barcode::UpcA(digits) => {
-                if digits.len() != 12 {
-                    return false;
-                }
-
-                check(digits, false)
-            }
-            Barcode::UpcE(digits) => {
-                if digits.len() != 6 {
-                    return false;
-                }
-
-                check(digits, false)
-            }
-        }
+        let (odd, even): (Vec<_>, Vec<_>) = self.0.iter().enumerate().partition(|&x| x.0 % 2 == 0);
+        let sum = even.iter().map(|x| *x.1 as u32).sum::<u32>() +
+            (odd.iter().map(|x| *x.1 as u32).sum::<u32>() * 3);
+        sum % 10 == 0
     }
 }
 
